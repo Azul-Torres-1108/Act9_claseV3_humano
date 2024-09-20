7aac36ce7e67ff965fa95de011c4aa78223f7ffb
Azul-Torres-1108 <167554917+Azul-Torres-1108@users.noreply.github.com>
2024-09-20 11:53:52 -0600

    Add files via upload

---
+++ humano1108.py
@@ -0,0 +1,52 @@
+print("Act 9 Clase humano")
+print("Mederyth Azul Torres Mat: 22308051281108")
+# Zona de class
+class Humano1108:
+    # Zona de atributos dentro de la clase
+    edad = 0
+    colordepelo = ""
+    estatura = 0
+    genero = ""
+    # Zona de funciones dentro de la clase
+    def correr1108(self,n):
+        print(f"{n} Esta corriendo uffff.......")
+
+    def hablar1108(self,n):
+        print(f"{n} Esta hablando de su cabello :D")
+
+    def midiendo1108(self,n):
+        print(f"{n} Esta midiendo su estatura")
+
+    def gritar1108(self,n):
+        print(f"{n} Esta gritando AAAAAAAAAAAA")
+# Zona de creacion de objetos   
+azul=Humano1108()
+Ed=Humano1108()
+# Zona de usando objetos
+print("Resultado para Azul")
+azul.edad=17
+azul.colordepelo="Cafe claro"
+azul.genero="mujer"
+azul.estatura=1.57
+print(f"edad: {azul.edad}")
+print(f"Color de pelo: {azul.colordepelo}")
+print(f"Genero: {azul.genero}")
+print(f"Estatura: {azul.estatura}")
+azul.correr1108("Azul")
+azul.hablar1108("Azul")
+azul.midiendo1108("Azul")
+azul.gritar1108("Azul")
+# ------------------------------------------------------
+print("Resultados para Ed")
+Ed.edad=20
+Ed.colordepelo="Cafe oscuro"
+Ed.genero="Hombre"
+Ed.estatura=1.65
+print(f"edad: {Ed.edad}")
+print(f"Color de pelo: {Ed.colordepelo}")
+print(f"Genero: {Ed.genero}")
+print(f"Estatura: {Ed.estatura}")
+Ed.correr1108("Ed")
+Ed.hablar1108("Ed")
+Ed.midiendo1108("Ed")
+Ed.gritar1108("Ed")
